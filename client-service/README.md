24f5ad206b0fd9a4c6bc9594af34f749c63e9367
Zenn <app.zenn@gmail.com>
2016-10-16 17:49:49 +0900

    client serviceにて、/companiesへアクセスすると会社情報を取得するAPIを作成

--- client-service/README.md
+++ client-service/README.md
@@ -1,24 +1,8 @@
-# README
+クライアントサービス
+===
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+# API
+```
+GET /companies
+リソースサービスにアクセスして、 `/companies` の情報を取得して返すエンドポイント
+```
---
+++ client-service/app/controllers/companies_controller.rb
@@ -0,0 +1,23 @@
+class CompaniesController < ApplicationController
+  def index
+    response = conn.get do |req|
+      req.url '/companies'
+      req.headers['HTTP_AUTHORIZATION'] = "Bearer #{access_token}"
+    end
+    render json: response.body
+  end
+
+  private
+
+  def conn
+    @conn ||= Faraday.new(url: 'http://lvh.me:5000') do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def access_token
+    ENV['ACCESS_TOKEN']
+  end
+end
